4bed5b90624722bc5b641a0e82eecd866f015b2c
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-27 10:23:15 +0000

    Brew formula update for batcher version v0.5.3

--- batcher.rb
+++ batcher.rb
@@ -2,16 +2,16 @@
 class Batcher < Formula
   desc "batcher does mass database updates and deletes in a database-friendly, concurrent manner"
   homepage ""
-  version "0.5.2"
+  version "0.5.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/SpokeyWheeler/batcher/releases/download/v0.5.2/batcher_0.5.2_Darwin_x86_64.tar.gz"
-    sha256 "2de1efa00d62ea1186268b34acdd3dc1179c991a8eb5f617b89233147ad9c349"
+    url "https://github.com/SpokeyWheeler/batcher/releases/download/v0.5.3/batcher_0.5.3_Darwin_x86_64.tar.gz"
+    sha256 "9681a8de7921fd1493c19dd5ab129e1ec53163331339ec47797a661a5fb855f9"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/SpokeyWheeler/batcher/releases/download/v0.5.2/batcher_0.5.2_Linux_x86_64.tar.gz"
-      sha256 "fe034ded5149d24607e610e96b5d9550964a4a2113a74855786637a00e5b2a78"
+      url "https://github.com/SpokeyWheeler/batcher/releases/download/v0.5.3/batcher_0.5.3_Linux_x86_64.tar.gz"
+      sha256 "7f16a42a9cb25229ec56620ac34221ceed9b193699d7f7b107430e925bab893c"
     end
   end
 
